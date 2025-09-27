429228154921611cf72bbf2e6f5c36f7938f6aca
Eder <eder.achaica@gmail.com>
2025-09-27 17:08:19 -0500

    Actions added

---
+++ src/server/actions.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { db } from "~/server/db";
+import { files_table } from "~/server/db/schema";
+import { UTApi } from "uploadthing/server";
+import { cookies } from "next/headers";
+
+const utApi = new UTApi();
+
+export async function deleteFile(fileId: number) {
+  const session = await auth();
+  if (!session.userId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+  const [file] = await db
+    .select()
+    .from(files_table)
+    .where(
+      and(eq(files_table.id, fileId), eq(files_table.ownerId, session.userId)),
+    );
+
+  if (!file) {
+    return {
+      error: "File not found",
+    };
+  }
+
+  const utpApiResult = await utApi.deleteFiles([
+    file.url.replace("https://x6o0nv7rr8.ufs.sh/f/", ""),
+  ]);
+
+  console.log(utpApiResult);
+
+  const dbDeleteResult = await db
+    .delete(files_table)
+    .where(eq(files_table.id, fileId));
+
+  console.log(dbDeleteResult);
+
+  const c = await cookies();
+  c.set("force-refresh", JSON.stringify(Math.random()));
+
+  return { success: true };
+}
